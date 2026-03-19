f7e1936a4b697bb04653a9da32e884050e7de2ee
TCWW <tcww3498@gmail.com>
2026-03-19 08:05:49 +0800

    Ignore build artifacts

--- build/CMakeFiles/core.dir/src/math.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/core.dir/src/math.cpp.o: /root/cs/c++/src/math.cpp \
- /usr/include/stdc-predef.h /root/cs/c++/include/math.h
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/cs/c++
-# Build directory: /root/cs/c++/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(core_tests "test")
-set_tests_properties(core_tests PROPERTIES  _BACKTRACE_TRIPLES "/root/cs/c++/CMakeLists.txt;36;add_test;/root/cs/c++/CMakeLists.txt;0;")
